abffa56a6d05203bedafad24596a312173eb33d3
jimenabelentorrilla <jiment4@hotmail.com>
2025-10-10 02:48:44 +0000

    feat(routes): add initial routing for pages

---
+++ src/app/(pages)/about-us/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold mb-4">Sobre Nosotros</h1>
+    </div>
+  )
+}
