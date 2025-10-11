20fd51bd459d886a3fc628f0e0015ecfbbe7eb5e
jimenabelentorrilla <jiment4@hotmail.com>
2025-10-11 00:51:10 +0000

    chore: install iconify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+"use client";
 
+import { Icon } from "@iconify-icon/react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
       <h1 className="text-4xl font-bold mb-4">Bienvenid@ a TripTap</h1>
       <p>Reserva tus pasajes de micro de manera fácil y rápida</p>
+      <Icon
+        icon="solar:bus-bold"
+        className="w-32 h-32 text-white-600" 
+      />
     </div>
   );
 }
