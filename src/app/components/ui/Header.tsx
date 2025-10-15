1fc54e06f41f27a0e6aebf0c6938647273ea287c
jimenabelentorrilla <jiment4@hotmail.com>
2025-10-15 01:23:59 +0000

    style: add styles to navbar

--- src/app/components/ui/Header.tsx
+++ src/app/components/ui/Header.tsx
@@ -4,41 +4,55 @@ import { useState } from "react";
 import Link from "next/link";
 import { Icon } from "@iconify-icon/react";
 
+import Image from "next/image";
+
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => setIsOpen(!isOpen);
 
   const navLinks = [
-    { name: "Home", href: "/" },
-    { name: "Reservas", href: "/reservations" },
+    { name: "Inicio", href: "/" },                  
     { name: "Sobre Nosotros", href: "/about-us" },
-    { name: "Login", href: "/login" },
-    { name: "Registro", href: "/register" },
+    { name: "Reservas", href: "/reservations" }
   ];
 
   return (
-    <header className="bg-gray-900 text-white shadow-md">
-      <nav className="container mx-auto flex items-center justify-between p-4">
-        <div className="text-2xl font-bold">TripTap</div>
+    <header className="bg-transparent shadow-md">
+      <nav className="container mx-auto flex items-center justify-between p-8">
+        <Image 
+          src="/images/Logo.png" 
+          alt="Logo"
+          width={140}
+          height={44} 
+          className="h-auto w-auto"
+        /> 
 
         {/* Desktop Links */}
-        <ul className="hidden md:flex gap-6">
+        <ul className="hidden md:flex gap-10">
           {navLinks.map((link) => (
             <li key={link.href}>
               <Link
                 href={link.href}
-                className="hover:text-blue-400 transition-colors"
+                className="hover:text-[#ED7A1C] transition-colors"
               >
                 {link.name}
               </Link>
             </li>
           ))}
+          <li>
+            <Link
+                href={"/login"}
+                className="bg-[#ED7A1C] text-white py-[10px] px-[28px] rounded-lg hover:bg-[#d96c17] transition-colors duration-300"
+            >
+              Ingresar 
+            </Link>
+          </li>
         </ul>
 
         {/* Hamburger Menu */}
         <button
-          className="md:hidden p-2 rounded hover:bg-gray-800 transition"
+          className="md:hidden p-2 rounded hover:bg-[#ED7A1C] transition"
           onClick={toggleMenu}
         >
           <Icon icon={isOpen ? "mdi:close" : "mdi:menu"} width="28" />
@@ -47,7 +61,7 @@ export default function Header() {
 
       {/* Mobile Menu */}
       {isOpen && (
-        <ul className="flex flex-col gap-4 p-4 bg-gray-800 md:hidden">
+        <ul className="flex flex-col gap-6 p-5 bg-gray-200 md:hidden">
           {navLinks.map((link) => (
             <li key={link.href}>
               <Link
@@ -59,6 +73,14 @@ export default function Header() {
               </Link>
             </li>
           ))}
+          <li>
+            <Link
+                href={"/login"}
+                className="bg-[#ED7A1C] text-white py-[10px] px-[28px] rounded-lg hover:bg-[#d96c17] transition-colors duration-300"
+            >
+              Ingresar 
+            </Link>
+          </li>
         </ul>
       )}
     </header>
